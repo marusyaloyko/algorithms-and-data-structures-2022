16ac1aeae7d4c395c5ebda9c6023e3bce8f5ee4e
Pastor <viruszold@gmail.com>
2022-10-07 09:54:45 +0300

    Первая практическая работа

--- students/0000001/0000001-p01/src/main/java/ru/mirea/practice/s0000001/Main.java
+++ students/0000001/0000001-p01/src/main/java/ru/mirea/practice/s0000001/Main.java
@@ -2,6 +2,6 @@ package ru.mirea.practice.s0000001;
 
 public final class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("первая практическая работа!");
     }
 }
