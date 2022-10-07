edf021185ae7f232a8f21b938ed11a164689fa89
Ivan Korsunov <ivan.corsunov@gmail.com>
2022-10-07 23:46:05 +0300

    Выполена 1я практическая работа

--- students/21K0637/21K0637-p01/src/main/java/ru/mirea/practice/s21K0637/Task3.java
+++ students/21K0637/21K0637-p01/src/main/java/ru/mirea/practice/s21K0637/Task3.java
@@ -3,7 +3,7 @@ package ru.mirea.practice.s21K0637;
 public class Task3 {
     public static void main(String[] args) {
         for (String i : args) {
-            System.out.println(i = " ");
+            System.out.println(i);
         }
     }
 }
