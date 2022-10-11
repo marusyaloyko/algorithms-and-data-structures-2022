479ec8fc1394812261ffb2b405b7d279d7afb883
k3nzzzo <ivanmikitenko23@gmail.com>
2022-10-11 10:52:10 +0300

    Практическая работа Микитенко Ивана

--- students/21K0664/21K0664-p01/src/main/java/ru/mirea/practice/s21K0664/Main.java
+++ students/21K0664/21K0664-p01/src/main/java/ru/mirea/practice/s21K0664/Main.java
@@ -2,6 +2,6 @@ package ru.mirea.practice.s21K0664;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("qwerty");
+        System.out.println("первая практическая работа");
     }
 }
--- students/21K0664/21K0664-p02/src/main/java/Main.java
+++ students/21K0664/21K0664-p02/src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("ad");
+        System.out.println("вторая практическая работа");
     }
 }
