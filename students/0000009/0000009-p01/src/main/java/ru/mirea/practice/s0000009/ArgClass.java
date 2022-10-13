85b85d233f4259a8909d56032547dcaf883218ef
IDerter <dturchan@mail.ru>
2022-10-13 19:08:46 +0300

    Первая практическая работа!

---
+++ students/0000009/0000009-p01/src/main/java/ru/mirea/practice/s0000009/ArgClass.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.s0000009;
+
+//Третья задача 1 практической работы
+public class ArgClass
+{
+    public static void main(String[] args)
+    {
+        for (String str : args){
+            System.out.println("аргумент = " + str);
+        }
+    }
+}
