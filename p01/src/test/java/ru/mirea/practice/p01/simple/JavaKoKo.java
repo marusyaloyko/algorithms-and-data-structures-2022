2f3f52a2bcefc1bf6e3db81f07fb43bc296d44e5
Pastor <viruszold@gmail.com>
2022-10-14 09:52:27 +0300

    Проверка кода. CodeStyle

--- p01/src/test/java/ru/mirea/practice/p01/simple/JavaKoKo.java
+++ p01/src/test/java/ru/mirea/practice/p01/simple/JavaKoKo.java
@@ -1,45 +1,17 @@
-
-
-
 package ru.mirea.practice.p01.simple;
 
-import java.io.Serializable;
+import java.text.MessageFormat;
+import java.util.Random;
+import java.util.StringTokenizer;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class JavaKoKo {
-    private final KoKo1 koko = new KoKo1();
-    int point = 0;
-
-    class KoKo1 {
-        void call() {
-            JavaKoKo.this.point = 1;
-        }
-    }
-
-    static class KoKo2 {
-        static class KoKo3 {
-
-        }
-    }
-
-    void call() {
-        koko.call();
-    }
-
-    enum Type implements Serializable {
-        ONE("Кол"), TWO("Двойка"), THREE("Тройка"), FOUR("Четверка"),
-        FIVE("Пятерка"), NOT_PRESENT("Не присутствовал");
-
-        public final String name;
-
-        Type(String name) {
-            this.name = name;
-        }
-    }
 
     public static void main(String[] args) {
-        JavaKoKo k = new JavaKoKo();
-        k.call();
-        System.out.println("Point: " + k.point);
-        System.out.println("Type : " + Type.ONE.name);
+        StringTokenizer tokenizer = new StringTokenizer("", ";!, ");
+        while (tokenizer.hasMoreElements()) {
+            tokenizer.nextToken();
+        }
+
     }
 }
