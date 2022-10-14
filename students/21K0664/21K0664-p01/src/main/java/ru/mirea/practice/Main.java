0cba03de777d5fc91c3da6769d1e1f98b801bfc6
k3nzzzo <ivanmikitenko23@gmail.com>
2022-10-14 13:56:31 +0300

    Практическая работа Микитенко Ивана

--- students/21K0664/21K0664-p01/src/main/java/ru/mirea/practice/Main.java
+++ students/21K0664/21K0664-p01/src/main/java/ru/mirea/practice/Main.java
@@ -1,31 +1,30 @@
-package ru.mirea.practice.s21K0664;
+package ru.mirea.practice;
 
 import java.util.Scanner;
 
 public class Main {
-    public static int fact(int n){
+    public static int fact(int n) {
         int result = 1;
-        for (int i = n; i > 1; i--){
+        for (int i = n; i > 1; i--) {
             result = result *  i;
         }
         return result;
     }
+
     public static void main(String[] args) {
 
         System.out.println("Задание 1.3");
         Scanner sc = new Scanner(System.in);
-        int n;
         int sum = 0;
-        double mid;
         System.out.print("Введите длину массива:");
-        n=sc.nextInt();
+        int n = sc.nextInt();
         int[] array = new int[n];
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = ((int)(Math.random() * n));
             System.out.println(array[i]);
             sum += array[i];
         }
-        mid  = ((double)sum)/n;
+        double mid = ((double)sum) / n;
         System.out.println("Сумма чисел: " + sum);
         System.out.println("Среднее арифмитическое: " + mid + "\n");
 
@@ -37,38 +36,37 @@ public class Main {
         System.out.println("Введите элементы массива:");
         int i = 0;
         int j = 0;
-        int t = 0;
-        int Max = 0;
+        int max = 0;
         while (i < size) {
             array1[i] = input.nextInt();
             i++;
         }
         System.out.println("Введите элементы массива:");
         while (j < size) {
-            System.out.print (" " + array1[j]);
+            System.out.print(" " + array1[j]);
             j++;
         }
-        int Min = array1[0];
-        while (t < size){
-            if (array1[t] > Max){
-                Max = array1[t];
+        int count = 0;
+        int min = array1[0];
+        while (count < size) {
+            if (array1[count] > max) {
+                max = array1[count];
             }
-            if (array1[t] < Max){
-                Min = array1[t];
+            if (array1[count] < max) {
+                min = array1[count];
             }
-            t++;
+            count++;
         }
-        System.out.print ("\nМаксимально число: " + Max + "\nМиинимальное число: " + Min + "\n");
+        System.out.print("\nМаксимально число: " + max + "\nМиинимальное число: " + min + "\n");
 
         System.out.println("Задание 1.5");
         System.out.print("Введите число: ");
         int num = input.nextInt();
         System.out.printf("Ваше число: %d \n", num);
         System.out.println("\nЗадание 1.6");
-        for (int k = 2; k < 10; k++){
+        for (int k = 2; k < 10; k++) {
             System.out.println("1/" + k);
         }
-
         System.out.println("\nЗадание 1.7");
         System.out.println("Введите число: ");
         int pr = input.nextInt();
