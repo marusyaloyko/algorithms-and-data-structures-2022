68f5aabcd5058a2e693abb79de8f4aaf25696b3e
Ilya Lelikov <lelikov-ilya@inbox.ru>
2022-10-15 02:08:45 +0300

    prct1,2(redact.fin?)

--- students/21K654/21K654-p03/src/main/java/ru/mirea/practice/task1/RandArr.java
+++ students/21K654/21K654-p03/src/main/java/ru/mirea/practice/task1/RandArr.java
@@ -6,9 +6,11 @@ import java.util.Random;
 public class RandArr {
     public static void main(String[] args) {
         double[] arr = new double[20];
+
         Random random = new Random();
-        for(int i = 0;i < arr.length;i++){
-            arr[i] = Math.random()*100;
+
+        for (int i = 0;i < arr.length;i++) {
+            arr[i] = Math.random() * 100;
             System.out.println(arr[i]);
         }
         Arrays.sort(arr);
@@ -17,9 +19,8 @@ public class RandArr {
             System.out.println(arr[i]);
         }
         System.out.println("||||||||||||||||||||||||||||||||");
-
-        for(int i = 0;i < arr.length;i++){
-            arr[i] = random.nextInt(100) ;
+        for (int i = 0;i < arr.length;i++) {
+            arr[i] = random.nextInt(100);
             System.out.println(arr[i]);
         }
         Arrays.sort(arr);
@@ -27,7 +28,5 @@ public class RandArr {
         for (int i = 0;i < arr.length;i++) {
             System.out.println(arr[i]);
         }
-
-
     }
 }
