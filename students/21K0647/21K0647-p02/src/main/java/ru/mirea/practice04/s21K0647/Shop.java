418cbe579b7820cbbfcbf17c8d174e960849f6bb
kodnennsy <lkvlkvlkv2003@yandex.ru>
2022-10-16 17:53:54 +0300

    Вторая практическая работа

---
+++ students/21K0647/21K0647-p02/src/main/java/ru/mirea/practice04/s21K0647/Shop.java
@@ -0,0 +1,30 @@
+package ru.mirea.practice04.s21K0647;
+
+import java.util.ArrayList;
+public class Shop {
+    ArrayList<Computer> C = new ArrayList<Computer>();
+
+    public void addComputer(Computer a) {
+        C.add(a);
+    }
+
+
+    public void deleteComputer(String model) {
+        for (Computer x : C) {
+            if(x.model.equals(model)){
+                C.remove(x);
+            }
+        }
+    }
+    public String searchComputer(String model) {
+        String n = "Not found";
+        for (Computer x : C) {
+            if (x.model.equals(model)) {
+                n =  "Model: " + x.model + "\tProcessor: " + x.processor + "\tMemory: " + x.memory + "\tBattery: " + x.battery;
+            }
+        }
+        return n;
+    }
+}
+
+
---
+++ students/21K0647/21K0647-p02/src/main/java/ru/mirea/practice05/s21K0647/Dog.java
@@ -0,0 +1,27 @@
+package ru.mirea.practice05.s21K0647;
+
+public class Dog {
+    String nickname;
+    int alt;
+
+    public Dog(String nickname, int alt){
+        this.nickname = nickname;
+        this.alt = alt;
+    }
+
+    public String getNickname(){ return nickname;}
+
+    public void setNickname(String nickname){ this.nickname = nickname;}
+
+    public int getAlt(){ return alt;}
+
+    public void setAlt(int alt){ this.alt = alt;}
+
+    public int ManAlt(){
+        return alt * 7;
+    }
+
+    public String ToString(){
+        return "Name: " + nickname + "\tAlt: " + alt;
+    }
+}
