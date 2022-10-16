d81b8a7264a8cbde97d6b2a3640e3fe195151607
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-10-16 15:17:34 +0300

    корректировки

--- students/21K0673/21K0673-p04/src/main/java/ru.mirea.practice/work2/Atelier.java
+++ students/21K0673/21K0673-p04/src/main/java/ru.mirea.practice/work2/Atelier.java
@@ -1,6 +1,6 @@
 package ru.mirea.practice.work2;
 
-public class Atelier extends Clothes{
+public class Atelier extends Clothes {
     public void dressWomen(Clothes[] clothes) {
 
     }
@@ -10,10 +10,10 @@ public class Atelier extends Clothes{
     }
 
     public static void main(String[] args) {
-        Clothes[] clothes = {
-                new Pants(EnumAtelier.L, 2000, "orange"),
-                new Skirt(EnumAtelier.XXS, 3000, "yellow"),
-                new Tie(EnumAtelier.XS, 1000, "orange"),
-                new TShirt(EnumAtelier.M, 500, "black")};
+        Clothes[] clothes = new Clothes[4];
+        clothes[0] = new Pants(EnumAtelier.L, 2000, "orange");
+        clothes[1] = new Skirt(EnumAtelier.XXS, 3000, "yellow");
+        clothes[2] = new Tie(EnumAtelier.XS, 1000, "orange");
+        clothes[3] = new TShirt(EnumAtelier.M, 500, "black");
     }
 }
