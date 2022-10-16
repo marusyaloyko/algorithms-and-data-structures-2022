4e3321af8fe5289616e6e9b86f0c9ec7fcd90490
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-10-16 14:42:59 +0300

    корректировки

--- students/21K0673/21K0673-p03/src/main/java/ru.mirea.practice/formatting/work3/Report.java
+++ students/21K0673/21K0673-p03/src/main/java/ru.mirea.practice/formatting/work3/Report.java
@@ -2,7 +2,7 @@ package ru.mirea.practice.formatting.work3;
 
 class Report {
 
-    static public void generateReport(Employee[] a) {
+    public static void generateReport(Employee[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.printf("%24s %-1.2f %n", a[i].getfullname(), a[i].getsalary());
         }
