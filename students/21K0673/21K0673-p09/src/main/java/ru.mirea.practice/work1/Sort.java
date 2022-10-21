4de083a18b54ed033e565b24f2d6810f3dba0f45
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-10-21 20:42:11 +0300

    исправления

--- students/21K0673/21K0673-p09/src/main/java/ru.mirea.practice/work1/Sort.java
+++ students/21K0673/21K0673-p09/src/main/java/ru.mirea.practice/work1/Sort.java
@@ -2,7 +2,7 @@ package ru.mirea.practice.work1;
 
 public class Sort {
     public static void main(String[] args) {
-        Student[] students = new Student[10];
+        Student[] students = new Student[9];
 
     }
 }
