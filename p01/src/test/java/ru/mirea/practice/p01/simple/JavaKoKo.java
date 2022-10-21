7a900e9d6fdc3304e8a695004c2bdc46c2740b4e
k3nzzzo <ivanmikitenko23@gmail.com>
2022-10-21 11:11:15 +0300

    Практическая работа Микитенко Ивана

--- p01/src/test/java/ru/mirea/practice/p01/simple/JavaKoKo.java
+++ p01/src/test/java/ru/mirea/practice/p01/simple/JavaKoKo.java
@@ -1,6 +1,9 @@
 package ru.mirea.practice.p01.simple;
 
+import java.text.MessageFormat;
+import java.util.Random;
 import java.util.StringTokenizer;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class JavaKoKo {
 
