c4e89ffd5753261466ca3cc30f613e4be01e2790
k3nzzzo <ivanmikitenko23@gmail.com>
2022-10-21 15:35:04 +0300

    Практическая работа Микитенко Ивана

--- students/21K0664/21K0664-p13/src/main/java/ru/mirea/workthirteen/task3/Test.java
+++ students/21K0664/21K0664-p13/src/main/java/ru/mirea/workthirteen/task3/Test.java
@@ -1,7 +1,13 @@
 package ru.mirea.workthirteen.task3;
 
+import java.util.StringTokenizer;
+
 public class Test {
+
     public static void main(String[] args) {
+        Adress a = new Adress("Страна Регион Город Улица Дом Корпус Квартира");
+        System.out.println(a + "\n");
+        a.print("Страна,Регион.Город\nУлица,Дом,Корпус.Квартира");
 
     }
 }
