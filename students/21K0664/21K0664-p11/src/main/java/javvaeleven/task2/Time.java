0c0839e17549c767c6ca7f6ba7739b717d42e74f
k3nzzzo <ivanmikitenko23@gmail.com>
2022-10-21 10:48:26 +0300

    Практическая работа Микитенко Ивана

--- students/21K0664/21K0664-p11/src/main/java/javvaeleven/task2/Time.java
+++ students/21K0664/21K0664-p11/src/main/java/javvaeleven/task2/Time.java
@@ -1,24 +1,24 @@
 package javvaeleven.task2;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Time {
-    public static void main(String[] args) {
-        Date date = new Date();
+    public static void main(String[] args) throws Exception {
+        Date now = new Date();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите дату в виде гггг-мм-дд: ");
+        String strdate = sc.nextLine();
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = s.parse(strdate);
+        sc.close();
 
-        //System.out.println(date.getDate() + " " + date.getYear() + " " + date.getMonth());
+        SimpleDateFormat smd = new SimpleDateFormat("MMM-dd-yyyy", Locale.ENGLISH);
+        System.out.println("Ваша дата: " + smd.format(date).toUpperCase());
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMMM dd");
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 2002);
-        cal.set(Calendar.MONTH, Calendar.AUGUST);
-        cal.set(Calendar.DAY_OF_MONTH, 31);
-        System.out.println(" Initialy set date:" + sdf.format(cal.getTime()));
-        cal.set(Calendar.MONTH, Calendar.SEPTEMBER);
-        System.out.println(" Date with month changed :" + sdf.format(cal.getTime()));
-        cal.set(Calendar.DAY_OF_MONTH, 30);
-        System.out.println(" Date with day changed:" + sdf.format(cal.getTime()));
+        System.out.println("Сегодняйшая дата: " + now);
+
     }
 }
