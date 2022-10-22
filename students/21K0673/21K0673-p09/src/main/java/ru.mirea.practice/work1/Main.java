c43a9d01afa4e3b5f4063e8c59765f165627b04f
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-10-22 19:28:30 +0300

    9 практика 1 номер

---
+++ students/21K0673/21K0673-p09/src/main/java/ru.mirea.practice/work1/Main.java
@@ -0,0 +1,9 @@
+package ru.mirea.practice.work1;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.Sort();
+        student.Print();
+    }
+}
