438078412b98f64d6553392fc7bf7d7a20cb42d1
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-10-23 15:30:05 +0300

    9 практика 2 номер

--- students/21K0673/21K0673-p09/src/main/java/ru.mirea.practice/work2/SortingStudentsByGPA.java
+++ students/21K0673/21K0673-p09/src/main/java/ru.mirea.practice/work2/SortingStudentsByGPA.java
@@ -1,4 +1,12 @@
 package ru.mirea.practice.work2;
 
-public class SortingStudentsByGPA implements Comparator{
+import java.util.Comparator;
+import java.lang.Comparable;
+
+public class SortingStudentsByGPA implements Comparator {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
 }
