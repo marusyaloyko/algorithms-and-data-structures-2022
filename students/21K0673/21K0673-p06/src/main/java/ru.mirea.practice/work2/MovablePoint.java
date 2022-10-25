ee0d7e2cb12c5d7bbbd6e9786e81b10a96097966
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-10-25 20:07:29 +0300

    9 практика 3 номер и 6 практика 2 номер

---
+++ students/21K0673/21K0673-p06/src/main/java/ru.mirea.practice/work2/MovablePoint.java
@@ -0,0 +1,33 @@
+package ru.mirea.practice.work2;
+
+public class MovablePoint implements Movable {
+    protected int x;
+    protected int y;
+    protected int xSpeed;
+    protected int ySpeed;
+    MovablePoint(int x,int y,int xSpeed,int ySpeed){
+        this.x=x;
+        this.y=y;
+        this.xSpeed=xSpeed;
+        this.ySpeed=ySpeed;
+    }
+    public String toString() {
+        return "MovablePoint: {x = "+x+", y = "+y+", xSpeed = "+xSpeed+", ySpeed = "+ySpeed+"}";
+    }
+
+    public void moveUp() {
+        this.y++;
+    }
+
+    public void moveDown() {
+        this.y--;
+    }
+
+    public void moveLeft() {
+        this.x--;
+    }
+
+    public void moveRight() {
+        this.x++;
+    }
+}
