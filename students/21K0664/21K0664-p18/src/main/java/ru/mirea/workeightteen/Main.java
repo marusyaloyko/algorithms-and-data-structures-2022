399f7813f54b88627a969f09620a1bd8c24b6d39
k3nzzzo <ivanmikitenko23@gmail.com>
2022-10-25 14:59:21 +0300

    Практическая работа Микитенко Ивана

---
+++ students/21K0664/21K0664-p18/src/main/java/ru/mirea/workeightteen/Main.java
@@ -0,0 +1,9 @@
+package ru.mirea.workeightteen;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Восемнадцатая практическая работа");
+    }
+}
