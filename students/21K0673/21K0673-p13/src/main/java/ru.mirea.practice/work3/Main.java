852a000afdb4d7deaade6f29fe8817b59e0019f4
MAMUER <mihnikolaenko12@yandex.ru>
2022-10-28 15:49:06 +0300

    13 практика

--- students/21K0673/21K0673-p13/src/main/java/ru.mirea.practice/work3/Main.java
+++ students/21K0673/21K0673-p13/src/main/java/ru.mirea.practice/work3/Main.java
@@ -2,6 +2,8 @@ package ru.mirea.practice.work3;
 
 public class Main {
     public static void main(String[] args) {
-
+        String strings = "Страна; Регион; Город; Улица. Дом/ Корпус, Квартира";
+        Address address = new Address(strings, false);
+        System.out.println(address.toString());
     }
 }
