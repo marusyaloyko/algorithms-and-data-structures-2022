20c39a5acecc52a34d0c935560b022edc0392624
MAMUER <mihnikolaenko12@yandex.ru>
2022-10-28 14:14:09 +0300

    13 практика

--- students/21K0673/21K0673-p11/src/main/java/ru.mirea.practice/work1to5/Main.java
+++ students/21K0673/21K0673-p11/src/main/java/ru.mirea.practice/work1to5/Main.java
@@ -3,9 +3,11 @@ package ru.mirea.practice.work1to5;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         System.out.println("Николаенко");
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMMM dd HH:mm:ss");
         Calendar cal = Calendar.getInstance();
@@ -24,5 +26,23 @@ public class Main {
         } else {
             System.out.println("Введённая дата меньше");
         }
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input year: ");
+        int year = in.nextInt();
+        cal.set(Calendar.YEAR, year);
+
+        System.out.print("Input month: ");
+        int month = in.nextInt();
+        cal.set(Calendar.MONTH, month);
+
+        System.out.print("Input day: ");
+        int day = in.nextInt();
+        cal.set(Calendar.DAY_OF_MONTH, day);
+
+        System.out.println("Calendar: " + sdf.format(cal.getTime()));
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println(estimatedTime);
     }
 }
---
+++ students/21K0673/21K0673-p13/src/main/java/ru.mirea.practice/work1/Main.java
@@ -0,0 +1,45 @@
+package ru.mirea.practice.work1;
+
+public class Main {
+    private String string;
+
+    Main(String string) {
+        this.string = string;
+    }
+
+    public void method() {
+        System.out.println(string.charAt(string.length() - 1));
+
+        if (string.endsWith("!!!")) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        if (string.startsWith("I like")) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        if (string.contains("Java")) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        System.out.println(string.indexOf("Java"));
+
+        System.out.println(string.replace('a', 'o'));
+
+        System.out.println(string.toUpperCase());
+
+        System.out.println(string.toLowerCase());
+
+        System.out.println(string.substring(1));
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main("I like Java!!!");
+        main.method();
+    }
+}
