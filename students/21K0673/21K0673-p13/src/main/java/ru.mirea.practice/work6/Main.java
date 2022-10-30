e941d6b5c0fc721bccbf6cbed72af3d0c171eec4
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-10-30 18:13:10 +0300

    13 практика

--- students/21K0673/21K0673-p13/src/main/java/ru.mirea.practice/work6/Main.java
+++ students/21K0673/21K0673-p13/src/main/java/ru.mirea.practice/work6/Main.java
@@ -1,7 +1,24 @@
 package ru.mirea.practice.work6;
 
-abstract class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+abstract class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        BufferedReader buff = new BufferedReader(new FileReader(fileName));
+
+        while (buff.ready()) {
+            String s = buff.readLine();
+
+            if (s.startsWith(args[0] + " ")) {
+                System.out.println(s.substring(args[0].length() + 1));
+            }
+        }
+        reader.close();
+        buff.close();
     }
 }
