908b146753995900151417fdf1a8ca4a48ca7c05
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-10-30 18:13:10 +0300

    13 практика

--- students/21K0673/21K0673-p13/src/main/java/ru.mirea.practice/work5/Main.java
+++ students/21K0673/21K0673-p13/src/main/java/ru.mirea.practice/work5/Main.java
@@ -2,6 +2,11 @@ package ru.mirea.practice.work5;
 
 abstract class Main {
     public static void main(String[] args) {
-
+        String strings = "+79123456789";
+        String string = "89123456789";
+        TelephoneNumber telephonenumber = new TelephoneNumber(strings);
+        TelephoneNumber number = new TelephoneNumber(string);
+        System.out.println(telephonenumber.toString());
+        System.out.println(number.toString());
     }
 }
