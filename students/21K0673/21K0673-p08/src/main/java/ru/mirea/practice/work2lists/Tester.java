214de7542fc1b3145eeb6f22fd58ce490cfcd9a8
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-01 13:15:34 +0300

    списки практика

---
+++ students/21K0673/21K0673-p08/src/main/java/ru/mirea/practice/work2lists/Tester.java
@@ -0,0 +1,14 @@
+package ru.mirea.practice.work2lists;
+
+abstract class Tester {
+    public static void main(String[] args) {
+        Group newList = new Group();
+        newList.addFirst(new Student("v", "zsehg", "seg", 3, "aarg"));
+        newList.addFirst(new Student("vrey", "zerysehg", "seeryg", 23, "aadfhrg"));
+        newList.displayList();
+        newList.removeFirst();
+        newList.removeLast();
+        newList.displayList();
+        newList.isEmpty();
+    }
+}
