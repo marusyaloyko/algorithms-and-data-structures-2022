957777232cd2e93b3e38dd03c423d410cdc86699
k3nzzzo <ivanmikitenko23@gmail.com>
2022-11-01 17:08:48 +0300

    Практическая работа Микитенко Ивана

--- students/21K0664/21K0664-p18/src/main/java/ru/mirea/workeightteen/task2/Exception2.java
+++ students/21K0664/21K0664-p18/src/main/java/ru/mirea/workeightteen/task2/Exception2.java
@@ -1,14 +1,22 @@
 package ru.mirea.workeightteen.task2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exception2 {
     public void exceptionDemo() {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.print("Enter an integer ");
-        String intString = myScanner.next();
-        myScanner.close();
-        int i = Integer.parseInt(intString);
-        System.out.println(2 / i);
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.print("Enter an integer ");
+            String intString = in.next();
+            in.close();
+            int i = Integer.parseInt(intString);
+            System.out.println(2 / i);
+        } catch (InputMismatchException e) {
+            System.out.println("Error");
+            in.nextInt();
+        } finally {
+            in.close();
+        }
     }
 }
