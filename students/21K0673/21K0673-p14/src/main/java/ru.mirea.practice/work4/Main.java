442c6a9e276f9ef223d238fd56a061123dd6a332
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-02 21:01:52 +0300

    14 практика 2-3 номера

---
+++ students/21K0673/21K0673-p14/src/main/java/ru.mirea.practice/work4/Main.java
@@ -0,0 +1,14 @@
+package ru.mirea.practice.work4;
+
+import java.util.regex.Pattern;
+
+abstract class Main {
+    public static void main(String[] args) {
+        String string = "soeiugh seoiugh aseoiguh";
+        Pattern pattern = Pattern.compile(" ");
+        String[] words = pattern.split(string);
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
