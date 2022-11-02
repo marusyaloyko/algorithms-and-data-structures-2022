ba4b7846131c9e14315da58103d376095d96354a
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-02 20:04:39 +0300

    14 практика 1 номер

--- students/21K0673/21K0673-p14/src/main/java/ru.mirea.practice/work1/Main.java
+++ students/21K0673/21K0673-p14/src/main/java/ru.mirea.practice/work1/Main.java
@@ -1,7 +1,14 @@
 package ru.mirea.practice.work1;
 
+import java.util.regex.Pattern;
+
 abstract class Main {
     public static void main(String[] args) {
-        System.out.println("f");
+        String string = "soeiugh seoiugh aseoiguh";
+        Pattern pattern = Pattern.compile(" ");
+        String[] words = pattern.split(string);
+        for (String word : words) {
+            System.out.println(word);
+        }
     }
 }
