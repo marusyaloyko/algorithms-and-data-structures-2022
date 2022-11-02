c5025a0d673455d1c7cafeefbcb2ada29b44e85a
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-02 21:05:58 +0300

    исправления

--- students/21K0673/21K0673-p14/src/main/java/ru.mirea.practice/work2/Main.java
+++ students/21K0673/21K0673-p14/src/main/java/ru.mirea.practice/work2/Main.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 abstract class Main {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("abcdefghijklmnopqrstuv18340");
-        Matcher matcher = pattern.matcher( "abcdefghijklmnoasdfasdpqrstuv18340" );
+        Matcher matcher = pattern.matcher("abcdefghijklmnoasdfasdpqrstuv18340");
         boolean bool = matcher.matches();
         System.out.println(bool);
     }
