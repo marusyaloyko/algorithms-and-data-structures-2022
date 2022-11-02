294163bf8a18464f5f1414d2c551c44a111c1e61
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-02 21:04:10 +0300

    исправления

--- students/21K0673/21K0673-p14/src/main/java/ru.mirea.practice/work4/Main.java
+++ students/21K0673/21K0673-p14/src/main/java/ru.mirea.practice/work4/Main.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 abstract class Main {
     public static void main(String[] args) {
-        String string = "soeiugh seoiugh aseoiguh";
+        String string = "(1 + 8) – 9 / 4";
         Pattern pattern = Pattern.compile(" ");
         String[] words = pattern.split(string);
         for (String word : words) {
