9ce3d9000357ea60058fc05ce7239e330864320f
Хлебников Андрей Александрович <Andrei.Khlebnikov@domrf.ru>
2022-11-03 20:36:53 +0300

    Исправление ошибок

--- p01/src/test/java/ru/mirea/practice/p01/simple/JavaKoKo.java
+++ p01/src/test/java/ru/mirea/practice/p01/simple/JavaKoKo.java
@@ -1,9 +1,6 @@
 package ru.mirea.practice.p01.simple;
 
-import java.text.MessageFormat;
-import java.util.Random;
 import java.util.StringTokenizer;
-import java.util.concurrent.ThreadLocalRandom;
 
 public final class JavaKoKo {
 
