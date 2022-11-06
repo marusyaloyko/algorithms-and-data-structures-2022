12c79fc00201f2e9e7560c9ce11357bed1438826
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-06 15:44:44 +0300

    20 и 22 практика

---
+++ students/21K0673/21K0673-p20/src/main/java/ru/mirea/practice/work4/MinMax.java
@@ -0,0 +1,38 @@
+package ru.mirea.practice.work4;
+
+public class MinMax<T> {
+    private T[] arr;
+    private int size;
+
+    MinMax(T[] arr1) {
+        arr = arr1;
+    }
+
+    public T[] getArr() {
+        return arr;
+    }
+
+    public void setArr(T[] arr) {
+        this.arr = arr;
+    }
+
+    public T min() {
+        T min = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            if ((Integer) arr[i] > (Integer) arr[i + 1]) {
+                min = arr[i + 1];
+            }
+        }
+        return min;
+    }
+
+    public T max() {
+        T max = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            if ((Integer) arr[i] < (Integer) arr[i + 1]) {
+                max = arr[i + 1];
+            }
+        }
+        return max;
+    }
+}
