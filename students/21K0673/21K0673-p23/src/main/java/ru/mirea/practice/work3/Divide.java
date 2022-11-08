987cdc45bc9303b4020c713a7b5dfa4c7d3f80f7
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-08 20:07:43 +0300

    23 практика

--- students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work3/Divide.java
+++ students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work3/Divide.java
@@ -1,5 +1,7 @@
 package ru.mirea.practice.work3;
 
+import java.util.regex.Pattern;
+
 public class Divide<T> {
     private String div;
 
@@ -11,8 +13,170 @@ public class Divide<T> {
         return div;
     }
 
-    public int evaluate(int i) {
-        return i;
+    public double evaluate(int number) {
+        Pattern p1 = Pattern.compile("[0-9]");
+        Pattern p2 = Pattern.compile("-");
+        Pattern p3 = Pattern.compile("\\+");
+        Pattern p4 = Pattern.compile("/");
+        Pattern p5 = Pattern.compile("\\*");
+
+        Pattern pattern = Pattern.compile(" ");
+        String[] words = pattern.split(div);
+        int buff = 0;
+        for (int i = 0; i < words.length; i += 2) {
+            if (!p1.matcher(words[i]).matches() & !p2.matcher(words[i]).matches() & !p3.matcher(words[i]).matches() & !p4.matcher(words[i]).matches() & !p5.matcher(words[i]).matches()) {
+                buff = i;
+                break;
+            }
+        }
+
+        double answer = 0;
+        switch (buff) {
+            case 0:
+                if (p2.matcher(words[3]).matches()) {
+                    answer = number + Integer.parseInt(words[4]);
+                    if (p2.matcher(words[1]).matches()) {
+                        answer = answer + Integer.parseInt(words[2]);
+                    } else if (p3.matcher(words[1]).matches()) {
+                        answer = answer - Integer.parseInt(words[2]);
+                    } else if (p4.matcher(words[1]).matches()) {
+                        answer = answer * Integer.parseInt(words[2]);
+                    } else if (p5.matcher(words[1]).matches()) {
+                        answer = answer / Integer.parseInt(words[2]);
+                    }
+                } else if (p3.matcher(words[3]).matches()) {
+                    answer = number - Integer.parseInt(words[4]);
+                    if (p2.matcher(words[1]).matches()) {
+                        answer = answer + Integer.parseInt(words[2]);
+                    } else if (p3.matcher(words[1]).matches()) {
+                        answer = answer - Integer.parseInt(words[2]);
+                    } else if (p4.matcher(words[1]).matches()) {
+                        answer = answer * Integer.parseInt(words[2]);
+                    } else if (p5.matcher(words[1]).matches()) {
+                        answer = answer / Integer.parseInt(words[2]);
+                    }
+                } else if (p4.matcher(words[3]).matches()) {
+                    answer = number * Integer.parseInt(words[4]);
+                    if (p2.matcher(words[1]).matches()) {
+                        answer = answer + Integer.parseInt(words[2]);
+                    } else if (p3.matcher(words[1]).matches()) {
+                        answer = answer - Integer.parseInt(words[2]);
+                    } else if (p4.matcher(words[1]).matches()) {
+                        answer = answer * Integer.parseInt(words[2]);
+                    } else if (p5.matcher(words[1]).matches()) {
+                        answer = answer / Integer.parseInt(words[2]);
+                    }
+                } else if (p5.matcher(words[3]).matches()) {
+                    answer = number / Integer.parseInt(words[4]);
+                    if (p2.matcher(words[1]).matches()) {
+                        answer = answer + Integer.parseInt(words[2]);
+                    } else if (p3.matcher(words[1]).matches()) {
+                        answer = answer - Integer.parseInt(words[2]);
+                    } else if (p4.matcher(words[1]).matches()) {
+                        answer = answer * Integer.parseInt(words[2]);
+                    } else if (p5.matcher(words[1]).matches()) {
+                        answer = answer / Integer.parseInt(words[2]);
+                    }
+                }
+                break;
+            case 2:
+                if (p2.matcher(words[3]).matches()) {
+                    answer = number + Integer.parseInt(words[4]);
+                    if (p2.matcher(words[1]).matches()) {
+                        answer = answer + Integer.parseInt(words[0]);
+                    } else if (p3.matcher(words[1]).matches()) {
+                        answer = answer - Integer.parseInt(words[0]);
+                    } else if (p4.matcher(words[1]).matches()) {
+                        answer = answer * Integer.parseInt(words[0]);
+                    } else if (p5.matcher(words[1]).matches()) {
+                        answer = answer / Integer.parseInt(words[0]);
+                    }
+                } else if (p3.matcher(words[3]).matches()) {
+                    answer = number - Integer.parseInt(words[4]);
+                    if (p2.matcher(words[1]).matches()) {
+                        answer = answer + Integer.parseInt(words[0]);
+                    } else if (p3.matcher(words[1]).matches()) {
+                        answer = answer - Integer.parseInt(words[0]);
+                    } else if (p4.matcher(words[1]).matches()) {
+                        answer = answer * Integer.parseInt(words[0]);
+                    } else if (p5.matcher(words[1]).matches()) {
+                        answer = answer / Integer.parseInt(words[0]);
+                    }
+                } else if (p4.matcher(words[3]).matches()) {
+                    answer = number * Integer.parseInt(words[0]);
+                    if (p2.matcher(words[3]).matches()) {
+                        answer = answer + Integer.parseInt(words[4]);
+                    } else if (p3.matcher(words[3]).matches()) {
+                        answer = answer - Integer.parseInt(words[4]);
+                    } else if (p4.matcher(words[3]).matches()) {
+                        answer = answer * Integer.parseInt(words[4]);
+                    } else if (p5.matcher(words[3]).matches()) {
+                        answer = answer / Integer.parseInt(words[4]);
+                    }
+                } else if (p5.matcher(words[3]).matches()) {
+                    answer = number / Integer.parseInt(words[0]);
+                    if (p2.matcher(words[3]).matches()) {
+                        answer = answer + Integer.parseInt(words[4]);
+                    } else if (p3.matcher(words[3]).matches()) {
+                        answer = answer - Integer.parseInt(words[4]);
+                    } else if (p4.matcher(words[3]).matches()) {
+                        answer = answer * Integer.parseInt(words[4]);
+                    } else if (p5.matcher(words[3]).matches()) {
+                        answer = answer / Integer.parseInt(words[4]);
+                    }
+                }
+                break;
+            case 4:
+                if (p2.matcher(words[1]).matches()) {
+                    answer = number + Integer.parseInt(words[0]);
+                } else if (p3.matcher(words[1]).matches()) {
+                    if (p2.matcher(words[3]).matches()) {
+                        answer = answer + Integer.parseInt(words[2]);
+                    } else if (p3.matcher(words[3]).matches()) {
+                        answer = answer - Integer.parseInt(words[2]);
+                    } else if (p4.matcher(words[3]).matches()) {
+                        answer = answer * Integer.parseInt(words[2]);
+                    } else if (p5.matcher(words[3]).matches()) {
+                        answer = answer / Integer.parseInt(words[2]);
+                    }
+                } else if (p3.matcher(words[1]).matches()) {
+                    answer = number - Integer.parseInt(words[0]);
+                    if (p2.matcher(words[3]).matches()) {
+                        answer = answer + Integer.parseInt(words[2]);
+                    } else if (p3.matcher(words[3]).matches()) {
+                        answer = answer - Integer.parseInt(words[2]);
+                    } else if (p4.matcher(words[3]).matches()) {
+                        answer = answer * Integer.parseInt(words[2]);
+                    } else if (p5.matcher(words[3]).matches()) {
+                        answer = answer / Integer.parseInt(words[2]);
+                    }
+                } else if (p4.matcher(words[1]).matches()) {
+                    answer = number * Integer.parseInt(words[0]);
+                    if (p2.matcher(words[3]).matches()) {
+                        answer = answer + Integer.parseInt(words[2]);
+                    } else if (p3.matcher(words[3]).matches()) {
+                        answer = answer - Integer.parseInt(words[2]);
+                    } else if (p4.matcher(words[3]).matches()) {
+                        answer = answer * Integer.parseInt(words[2]);
+                    } else if (p5.matcher(words[3]).matches()) {
+                        answer = answer / Integer.parseInt(words[2]);
+                    }
+                } else if (p5.matcher(words[1]).matches()) {
+                    answer = number / Integer.parseInt(words[0]);
+                    if (p2.matcher(words[3]).matches()) {
+                        answer = answer + Integer.parseInt(words[2]);
+                    } else if (p3.matcher(words[3]).matches()) {
+                        answer = answer - Integer.parseInt(words[2]);
+                    } else if (p4.matcher(words[3]).matches()) {
+                        answer = answer * Integer.parseInt(words[2]);
+                    } else if (p5.matcher(words[3]).matches()) {
+                        answer = answer / Integer.parseInt(words[2]);
+                    }
+                }
+                break;
+        }
+        System.out.println(answer);
+        return answer;
     }
 
     @Override
--- students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work3/Main.java
+++ students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work3/Main.java
@@ -2,6 +2,6 @@ package ru.mirea.practice.work3;
 
 abstract class Main {
     public static void main(String[] args) {
-        new Subtract(new Multiply(new Const(5), new Variable("x")), new Const(1)).evaluate(5);
+        new Subtract(new Multiply(new Const(2), new Variable("x")), new Const(3)).evaluate(5);
     }
 }
