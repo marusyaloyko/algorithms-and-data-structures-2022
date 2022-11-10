8e089cd23200b12f755b3bee4ab9a0d24ddceaab
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-10 10:10:01 +0300

    24 практика

--- students/21K0673/21K0673-p24/src/main/java/ru/mirea/practice/work3/File.java
+++ students/21K0673/21K0673-p24/src/main/java/ru/mirea/practice/work3/File.java
@@ -1,8 +1,22 @@
 package ru.mirea.practice.work3;
 
 public class File {
-    private boolean New;
-    private boolean Open;
-    private boolean Save;
-    private boolean Exit;
+    private Create create;
+
+    public void New(String string) {
+        create.CreateNew(string);
+    }
+
+    public void Open(String string) {
+        create.CreateOpen(string);
+    }
+
+    public void Save() {
+        System.out.println("Файлы сохранены");
+    }
+
+    public void Exit() {
+        System.out.println("Теперь вы можете открыть другой файл");
+    }
+
 }
--- students/21K0673/21K0673-p24/src/main/java/ru/mirea/practice/work3/Main.java
+++ students/21K0673/21K0673-p24/src/main/java/ru/mirea/practice/work3/Main.java
@@ -1,5 +1,11 @@
 package ru.mirea.practice.work3;
 
 abstract class Main {
-
+    public static void main(String[] args) {
+        File file = new File();
+        file.New("Text.txt");
+        file.Open("Text.txt");
+        file.Save();
+        file.Exit();
+    }
 }
