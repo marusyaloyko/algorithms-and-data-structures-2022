b4f69f0f3f7d13f574231caf44b8b3eb3259b665
N-T1 <nastya.1227@yandex.ru>
2022-11-12 18:53:35 +0300

    Первая и вторая практические работы

--- students/21K0706/21K0706-p01/src/main/java/ru/mirea/practice/s21k0706/Number14.java
+++ students/21K0706/21K0706-p01/src/main/java/ru/mirea/practice/s21k0706/Number14.java
@@ -7,9 +7,8 @@ public final class Number14 {
 
     public static void main(String[] args) {
         System.out.println("The first 10 numbers of the harmonic series: ");
-        for (int i = 0; i < 10; i++) {
-            double k = i;
-            System.out.printf("%.2f\n", 1 / (k + 1));
+        for (double i = 0; i < 10; i++) {
+            System.out.printf("%.2f%n", 1 / (i + 1));
         }
     }
 }
