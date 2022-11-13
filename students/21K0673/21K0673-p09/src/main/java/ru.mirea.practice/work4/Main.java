33420f4abee4fd469dc974d165c605fef99fc3a0
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-13 13:41:40 +0300

    исправления

--- students/21K0673/21K0673-p09/src/main/java/ru.mirea.practice/work4/Main.java
+++ students/21K0673/21K0673-p09/src/main/java/ru.mirea.practice/work4/Main.java
@@ -2,7 +2,8 @@ package ru.mirea.practice.work4;
 
 abstract class Main {
     public static void main(String[] args) {
-        Work[] mains = new Work[]{new Work(8), new Work(4), new Work(1), new Work(3), new Work(3)};
+        Work[] mains = new Work[]{new Work(8), new Work(4), new Work(1), new Work(3),
+                new Work(3)};
         Work main = new Work(0);
         main.compareTo(mains);
 
