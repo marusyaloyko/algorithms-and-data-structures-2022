9e49a47eb3fb849566b74ce98ce1ac5f128923aa
Ilya <ilya.trishkin.03@mail.ru>
2022-11-13 14:46:24 +0300

    Тришкин Илья написал первую практическую

--- students/21K0708/21K0708-p01/src/main/java/ru/mirea/practice/s21K0708/Arguments.java
+++ students/21K0708/21K0708-p01/src/main/java/ru/mirea/practice/s21K0708/Arguments.java
@@ -1,7 +1,9 @@
 package ru.mirea.practice.s21K0708;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Arguments {
-    public static void main (String [] args){
+    public static void main (String @NotNull [] args){
         for (String arg : args) System.out.println(arg);
     }
 }
