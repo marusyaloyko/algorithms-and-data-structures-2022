c103a2641699fd3eaccee7a1d16152af4ee635ee
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-13 17:23:49 +0300

    исправления

--- students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work1/arrayqueueTest.java
+++ students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work1/arrayqueueTest.java
@@ -1,6 +1,6 @@
 package ru.mirea.practice.work1;
 
-abstract class ArrayqueueTest {
+abstract class arrayqueueTest {
     public static void main(String[] args) {
         ArrayQueue q = new ArrayQueue();
         q.dequeue();
--- students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work1/arrayqueuemoduleTest.java
+++ students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work1/arrayqueuemoduleTest.java
@@ -1,6 +1,6 @@
 package ru.mirea.practice.work1;
 
-abstract class ArrayqueuemoduleTest {
+abstract class arrayqueuemoduleTest {
     public static void main(String[] args) {
         ArrayQueue q = new ArrayQueue();
         q.dequeue();
