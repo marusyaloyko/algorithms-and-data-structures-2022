9f5c53039f2ffc980d8fd936faf3fbdbcb9bdddd
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-13 17:02:10 +0300

    исправления

--- students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work1/AdtTest.java
+++ students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work1/AdtTest.java
@@ -1,6 +1,6 @@
 package ru.mirea.practice.work1;
 
-abstract class ArrayqueueadtTest {
+abstract class AdtTest {
     public static void main(String[] args) {
         System.out.println("a");
     }
--- students/21K0673/21K0673-p23/src/main/java/ru/mirea/practice/work1/arrayqueueadtTest.java
+++
@@ -1,7 +0,0 @@
-package ru.mirea.practice.work1;
-
-abstract class ArrayqueueadtTest {
-    public static void main(String[] args) {
-        System.out.println("a");
-    }
-}
