a3c76af6542502ea6c528af5ef84d9c9dbb2af3a
Ilya <ilya.trishkin.03@mail.ru>
2022-11-14 22:48:52 +0300

    Тришкин Илья написал первую практическую

--- students/21K0708/21K0708-p01/src/main/java/ru/mirea/practice/s21K0708/Arguments.java
+++ students/21K0708/21K0708-p01/src/main/java/ru/mirea/practice/s21K0708/Arguments.java
@@ -3,7 +3,7 @@ package ru.mirea.practice.s21K0708;
 import org.jetbrains.annotations.NotNull;
 
 public class Arguments {
-    public static void main (String @NotNull [] args){
+    public static void main(String @NotNull [] args) {
         for (String arg : args) System.out.println(arg);
     }
 }
--- students/21K0708/21K0708-p01/src/main/java/ru/mirea/practice/s21K0708/Input.java
+++ students/21K0708/21K0708-p01/src/main/java/ru/mirea/practice/s21K0708/Input.java
@@ -3,12 +3,12 @@ package ru.mirea.practice.s21K0708;
 import java.util.Scanner;
 
 public class Input {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int[] Array = new int[size];
 
-        for (int i = 0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             Array[i] = sc.nextInt();
         }
 
@@ -16,29 +16,29 @@ public class Input {
         int max = 0;
         int min = Array[0];
         int j = 0;
-        do{
+        do {
             sum = sum + Array[j];
 
-            if (Array[j]>max) {
+            if (Array[j] > max) {
                 max = Array[j];
-            } else if (Array[j]<min) {
+            } else if (Array[j] < min) {
                 min = Array[j];
             }
 
             j++;
-        } while (j<size);
+        } while (j < size);
         System.out.printf("sum = %d\nmax = %d\nmin = %d\n", sum, max, min);
 
         sum = 0;
         max = 0;
         min = Array[0];
         j = 0;
-        while (j<size){
+        while (j < size) {
             sum = sum + Array[j];
 
-            if (Array[j]>max) {
+            if (Array[j] > max) {
                 max = Array[j];
-            } else if (Array[j]<min) {
+            } else if (Array[j] < min) {
                 min = Array[j];
             }
 
