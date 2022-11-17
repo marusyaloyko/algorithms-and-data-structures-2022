81056d41938cf274dc17faa84d94763146e96e4f
Pinem3 <n.m.post381@gmail.com>
2022-11-17 20:12:40 +0300

    Отправлена первая домашняя работа

--- students/21K0598/21K0598-p01/src/main/java/ru/mirea/practice/One4.java
+++ students/21K0598/21K0598-p01/src/main/java/ru/mirea/practice/One4.java
@@ -3,7 +3,7 @@ package ru.mirea.practice;
 public class One4 {
     public static void main(String[] args) {
         float res;
-        for (float i = 1f; i < 11; i++){
+        for (float i = 1f; i < 11; i++) {
             res = 1 / i;
             System.out.printf("%.2f", res);
             System.out.print(' ');
