b97db15fdc2a3ac86d087474e9aae4942d710174
mishnevo <pepefrog240@gmail.com>
2022-11-18 01:24:12 +0300

    Третья практическая. Попробовал реализовать вышеописанное

--- students/21K0667/21K0667-p03/src/main/java/ru/mirea/practice/form/task2/ConvertorShop.java
+++ students/21K0667/21K0667-p03/src/main/java/ru/mirea/practice/form/task2/ConvertorShop.java
@@ -1,30 +1,20 @@
 package ru.mirea.practice.form.task2;
 
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 abstract class ConvertorShop {
+    String language;
+    String country;
+    double course;
+    double sum;
 
-    public static double getEuroToDollar(double euro) {
-        return euro * 1.04;
-    }
-
-    public static double getDollarToEuro(double dollar) {
-        return dollar / 1.04;
-    }
-
-    public static double getEuroToRub(double euro) {
-        return euro * 63.05;
-    }
-
-    public static double getRubToEuro(double dollar) {
-        return dollar / 63.05;
-    }
-
-    public static double getDollarToRub(double dollar) {
-        return dollar * 60.42;
-    }
-
-    public static double getRubToDollar(double dollar) {
-        return dollar / 60.42;
+    public static String convert(String language, String country, double course, double sum) {
+        Locale region = new Locale(language, country);
+        double converted = sum * course;
+        NumberFormat out = NumberFormat.getCurrencyInstance(region);
+        return out.format(converted);
     }
 
 
--- students/21K0667/21K0667-p03/src/main/java/ru/mirea/practice/form/task2/InternetShop.java
+++ students/21K0667/21K0667-p03/src/main/java/ru/mirea/practice/form/task2/InternetShop.java
@@ -1,51 +1,26 @@
 package ru.mirea.practice.form.task2;
 
 
-import java.text.NumberFormat;
-import java.util.Locale;
 import java.util.Scanner;
 
 abstract class InternetShop {
     public static void main(String[] args) {
+        String language;
+        String land;
+        double course;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Write your language code: ");
+            language = scanner.nextLine();
+            System.out.println("Write your land code");
+            land = scanner.nextLine();
+            System.out.println("Exchange rate from dollar to your currency:");
+            course = scanner.nextDouble();
+        }
         double bread = 0.5;
         double milk = 1.5;
         double cheese = 2;
-
-        System.out.println("What currency do you want to pay with: ");
-        System.out.println("1: Rub, 2: Dollar, 3: Euro");
-        try (Scanner scanner = new Scanner(System.in)) {
-            int choiceOfCurrency = scanner.nextInt();
-
-            switch (choiceOfCurrency) {
-                case 1:
-                    bread = ConvertorShop.getDollarToRub(bread);
-                    milk = ConvertorShop.getDollarToRub(milk);
-                    cheese = ConvertorShop.getDollarToRub(cheese);
-                    Locale ru = new Locale("ru", "RU");
-                    NumberFormat format1 = NumberFormat.getCurrencyInstance(ru);
-                    System.out.println(format1.format(bread) + " - bread");
-                    System.out.println(format1.format(milk) + " - milk");
-                    System.out.println(format1.format(cheese) + " - cheese");
-                    break;
-                case 2:
-                    NumberFormat format2 = NumberFormat.getCurrencyInstance(Locale.US);
-                    System.out.println(format2.format(bread) + " - bread");
-                    System.out.println(format2.format(milk) + " - milk");
-                    System.out.println(format2.format(cheese) + " - cheese");
-                    break;
-                case 3:
-                    bread = ConvertorShop.getDollarToEuro(bread);
-                    cheese = ConvertorShop.getDollarToEuro(cheese);
-                    milk = ConvertorShop.getDollarToEuro(milk);;
-                    NumberFormat format3 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-                    System.out.println(format3.format(bread) + " - bread");
-                    System.out.println(format3.format(milk) + " - milk");
-                    System.out.println(format3.format(cheese) + " - cheese");
-                    break;
-                default:
-                    System.out.println("Wrong");
-                    break;
-            }
-        }
+        System.out.println("bread: " + ConvertorShop.convert(language, land, course, bread));
+        System.out.println("milk: " + ConvertorShop.convert(language, land, course, milk));
+        System.out.println("cheese: " + ConvertorShop.convert(language, land, course, cheese));
     }
 }
