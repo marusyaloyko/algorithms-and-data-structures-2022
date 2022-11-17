34253e2dfe402313f5eafaa741b1060cb683f5c9
Pinem3 <n.m.post381@gmail.com>
2022-11-17 20:36:59 +0300

    Отправлена первая домашняя работа

--- students/21K0598/21K0598-p01/src/main/java/ru/mirea/practice/One5.java
+++ students/21K0598/21K0598-p01/src/main/java/ru/mirea/practice/One5.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 abstract class One5 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите числоб факториал которого вы желаете найти: ");
-        int num = sc.nextInt();
-        int fact = 1;
-        for (int i = num; i > 0; i--) {
-            fact *= i;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Введите числоб факториал которого вы желаете найти: ");
+            int num = sc.nextInt();
+            int fact = 1;
+            for (int i = num; i > 0; i--) {
+                fact *= i;
+            }
+            System.out.print("Факториал числа ");
+            System.out.print(num);
+            System.out.print(" = ");
+            System.out.print(fact);
         }
-        System.out.print("Факториал числа ");
-        System.out.print(num);
-        System.out.print(" = ");
-        System.out.print(fact);
-        sc.close();
     }
 }
