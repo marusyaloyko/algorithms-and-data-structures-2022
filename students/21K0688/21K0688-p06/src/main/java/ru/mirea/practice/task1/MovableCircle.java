81afd353a20ab244b7aa012f2502e22ed8079104
Arseny Revunov <seny-pnz2003@mail.ru>
2022-11-18 11:04:44 +0300

    Практическая работа 6. Добавил радиус в MovableCircle

--- students/21K0688/21K0688-p06/src/main/java/ru/mirea/practice/task1/MovableCircle.java
+++ students/21K0688/21K0688-p06/src/main/java/ru/mirea/practice/task1/MovableCircle.java
@@ -2,6 +2,15 @@ package ru.mirea.practice.task1;
 
 public class MovableCircle implements Movable {
     private MovablePoint center;
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 
     public MovableCircle(int x, int y, int xspeed, int yspeed) {
         center = new MovablePoint(x, y, xspeed, yspeed);
