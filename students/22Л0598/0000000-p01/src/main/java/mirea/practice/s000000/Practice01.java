71da986f18a2623c70259c9a685a0a0a959ba768
marusyaloyko <115215237+marusyaloyko@users.noreply.github.com>
2022-11-30 23:22:39 +0300

    message baluyus

---
+++ students/22Л0598/0000000-p01/src/main/java/mirea/practice/s000000/Practice01.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.s000000;
+
+public final class Practice01 {
+
+    private Practice01() {
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
